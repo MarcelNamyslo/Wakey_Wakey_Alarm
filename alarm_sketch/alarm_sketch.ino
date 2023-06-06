4f64bc25812dd5579b5ea5d734e2ffdab9c9500f
Mercyrion <marcel.nam@web.de>
2023-06-06 16:18:40 +0200

    update index html, add new js functions to receive data from ino

--- alarm_sketch/alarm_sketch.ino
+++ alarm_sketch/alarm_sketch.ino
@@ -3,13 +3,13 @@
 #include <ESPAsyncWebServer.h>
 
 
-const char* ssid = "Hedon Misafir";
-//const char* ssid = "Point";
-//const char* password =  "Point2424";
-const char* password =  "hedon2020";
+//const char* ssid = "Hedon Misafir";
+const char* ssid = "Point";
+const char* password =  "Point2424";
+//const char* password =  "hedon2020";
 
 AsyncWebServer server(80);
-
+String textToSend;
 //not used
 void handleSaveData(AsyncWebServerRequest *request) {
   if (request->method() == HTTP_POST) {
@@ -47,21 +47,21 @@ void handlePostRequest(AsyncWebServerRequest *request) {
         return;
       }
       //file.close();
-     
-  
-      
+
+
+
       // Do something with the received data
       Serial.println("Received data: " + data);
       file.println(data);
 
-      
+
       File file1 = SPIFFS.open("/data.txt", "r");
       String data1 = file1.readString();
       file1.close();
       Serial.println("txt data: " + data1);
-  
-  
-  file.close();
+
+
+      file.close();
     } else {
       request->send(400, "text/plain", "Invalid request");
     }
@@ -83,16 +83,18 @@ void handleGetData(AsyncWebServerRequest *request) {
   request->send(200, "text/plain", data);
 }
 
+void handleText(AsyncWebServerRequest *request) {
+  if (request->hasParam("text", true)) {
+    textToSend = request->getParam("text", true)->value();
+    request->send(200, "text/plain", textToSend);
+  }
+}
+
 
 
 
 void setup() {
   Serial.begin(115200);
-
-  if (!SPIFFS.begin()) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
 
   WiFi.begin(ssid, password);
 
@@ -103,6 +105,11 @@ void setup() {
 
   Serial.println(WiFi.localIP());
 
+  if (!SPIFFS.begin()) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html", "text/html");
   });
@@ -112,15 +119,11 @@ void setup() {
     handlePostRequest(request);
   });
 
+  server.on("/sendtext", HTTP_POST, [](AsyncWebServerRequest * request) {
+    handleText(request);
+  });
 
-   server.on("/back", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", );
-  });
-  server.on("/getData", HTTP_GET, [](AsyncWebServerRequest * request) {
-    // Handle the POST request
-    handleGetData(request);
-  });
-  //server.on("/getData", HTTP_GET, handleGetData);
+
 
   server.begin();
 }
